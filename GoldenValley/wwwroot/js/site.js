fe3beae992e57a5ce9d93861b62b9f84084c28e1
DSQuintanaP <quintana.santiago1018@gmail.com>
2024-10-25 13:16:06 -0500

    DataTable working

--- GoldenValley/wwwroot/js/site.js
+++ GoldenValley/wwwroot/js/site.js
@@ -2,6 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-let table = new datatable('#dt2', {
-    pageLength: 15
-})
+//let table = new datatable('#dt2', {
+//    pageLength: 15
+//})
+$(document).ready(function () {
+    $('#mainReserva').DataTable();
+});
